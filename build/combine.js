1a846f2e8b3094601ab4d254a65d7078112dc27f
b-lack <gerrit.balindt@b-lack.io>
2022-10-03 14:24:47 +0200

    clear ,  from dereferenced

--- build/combine.js
+++ build/combine.js
@@ -7,6 +7,7 @@ import $RefParser from '@apidevtools/json-schema-ref-parser'
 const argv = process.argv.slice(2);
 
 const VERSION = argv[0];
+if(!VERSION) console.error('No version given');
 const SCHEMANAME = 'vinv-tree';
 
 const distDirectory = `./dist/${VERSION}`;
@@ -23,7 +24,33 @@ const bundle = async (version, schemaName) => {
     return $RefParser.bundle(`./src/${version}/${schemaName}.json`);
 }
 
+function clearUnsupportedKeywords(schema, unsupportedKeywords, mute, multitype){
+    var keys = Object.keys(schema);
+
+    if(!multitype && schema.type  && typeof schema.type == 'object') {
+        if(schema.minItems && schema.type.includes('null'))
+            schema.minItems = 0;
+        schema.type = schema.type.find(element => element !== "null");
+    }
+
+    for(var key of keys){
+
+        if(unsupportedKeywords.includes(key) && !mute){
+            delete schema[key];
+        }
+
+        if(typeof schema[key] == "object"){
+            schema[key] = clearUnsupportedKeywords(schema[key], unsupportedKeywords, false, multitype);
+        }
+
+    }
+    return schema;
+}
+
+
+
 const schema = await dereference(VERSION, SCHEMANAME);
+schema.properties = clearUnsupportedKeywords(schema.properties, ['$id', '$defs', '$schema'], true, true);
 await writeFileAsync(`${distDirectory}/${SCHEMANAME}.dereferenced.min.json`, JSON.stringify(schema))
 
 const bundled_schema = await bundle(VERSION, SCHEMANAME);
