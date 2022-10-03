ac16b1ae338f7cc459903b57dda788513e37f47a
b-lack <gerrit.balindt@b-lack.io>
2022-10-03 20:45:22 +0200

    renew schema

--- build/combine.js
+++ build/combine.js
@@ -4,6 +4,11 @@ const writeFileAsync = promisify(fs.writeFile)
 
 import $RefParser from '@apidevtools/json-schema-ref-parser'
 
+import Ajv from "ajv"
+import addFormats from "ajv-formats"
+
+let ajv;
+
 const argv = process.argv.slice(2);
 
 const VERSION = argv[0];
@@ -12,19 +17,17 @@ const SCHEMANAME = 'vinv-tree';
 
 const distDirectory = `./dist/${VERSION}`;
 
-
-console.log(argv);
-
 const VERSIONCODE = process.env.npm_package_version
 
 const dereference = async (version, schemaName) => {
     return await $RefParser.dereference(`./src/${version}/${schemaName}.json`);
 }
 const bundle = async (version, schemaName) => {
-    return $RefParser.bundle(`./src/${version}/${schemaName}.json`);
+    return await $RefParser.bundle(`./src/${version}/${schemaName}.json`);
 }
 
-function clearUnsupportedKeywords(schema, unsupportedKeywords, mute, multitype){
+function clearUnsupportedKeywords(schema, unsupportedKeywords, multitype, anchor = null, anchors = []){
+    schema = JSON.parse(JSON.stringify(schema));
     var keys = Object.keys(schema);
 
     if(!multitype && schema.type  && typeof schema.type == 'object') {
@@ -33,26 +36,62 @@ function clearUnsupportedKeywords(schema, unsupportedKeywords, mute, multitype){
         schema.type = schema.type.find(element => element !== "null");
     }
 
-    for(var key of keys){
+    if(typeof anchor === 'string' && anchor !== 'properties'){
+        anchors = [...anchors, anchor]
+        schema["$anchor"] = anchors.join('.');
+    }
 
-        if(unsupportedKeywords.includes(key) && !mute){
+    for(var key of keys){
+        if(unsupportedKeywords.includes(key)){
             delete schema[key];
         }
 
         if(typeof schema[key] == "object"){
-            schema[key] = clearUnsupportedKeywords(schema[key], unsupportedKeywords, false, multitype);
+            schema[key] = clearUnsupportedKeywords(schema[key], unsupportedKeywords, multitype, key, anchors);
         }
 
     }
+
+    
     return schema;
 }
 
 
 
-const schema = await dereference(VERSION, SCHEMANAME);
-schema.properties = clearUnsupportedKeywords(schema.properties, ['$defs'], true, true);
-await writeFileAsync(`${distDirectory}/${SCHEMANAME}.dereferenced.min.json`, JSON.stringify(schema))
+
+
+
+let schema = await dereference(VERSION, SCHEMANAME);
+
+schema.properties = clearUnsupportedKeywords(schema.properties, ['$id', '$schema'], true, true, []);
+schema = clearUnsupportedKeywords(schema, ['$defs'], true, true, []);
+
+ajv = new Ajv({strict:false, allErrors:true});
+addFormats(ajv)
+
+const compiledSchema = ajv.addSchema(schema, 'dereferenced');
+console.log(ajv.getSchema('https://raw.githubusercontent.com/vinv-group/vinv-tree/main/dist/0.0.1/vinv-tree.json#location').schema);
+if(compiledSchema.errors){
+    console.error(compiledSchema.errors);
+}else{
+    await writeFileAsync(`${distDirectory}/${SCHEMANAME}.dereferenced.min.json`, JSON.stringify(schema))
+}
+
+
 
 const bundled_schema = await bundle(VERSION, SCHEMANAME);
-await writeFileAsync(`${distDirectory}/${SCHEMANAME}.json`, JSON.stringify(bundled_schema, null, 2))
-await writeFileAsync(`${distDirectory}/${SCHEMANAME}.min.json`, JSON.stringify(bundled_schema))
+bundled_schema.properties = clearUnsupportedKeywords(bundled_schema.properties, ['$id', '$schema'], true, true, []);
+// DARF NICHT 2 mal hinzugefügt werden
+ajv = new Ajv({strict:false, allErrors:true});
+addFormats(ajv)
+const bundledSchema = ajv.addSchema(bundled_schema, 'bundled_schema');
+console.log(JSON.stringify(ajv.getSchema('bundled_schema#location').schema, null, 2));
+
+if(bundledSchema.errors){
+    console.error(bundledSchema.errors);
+}else{
+    //await writeFileAsync(`${distDirectory}/${SCHEMANAME}.json`, JSON.stringify(bundled_schema, null, 2))
+    await writeFileAsync(`${distDirectory}/${SCHEMANAME}.min.json`, JSON.stringify(bundled_schema))
+}
+
+console.log('done');
